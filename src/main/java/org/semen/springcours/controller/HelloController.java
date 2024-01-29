b7ccc871cdf5110981f8f92cf78429b8fae9f877
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2024-01-29 14:09:07 +0300

    Начало сприн бут проекта

---
+++ src/main/java/org/semen/springcours/controller/HelloController.java
@@ -0,0 +1,17 @@
+package org.semen.springcours.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println(this.hello);
+        return "/hello";
+    }
+}
